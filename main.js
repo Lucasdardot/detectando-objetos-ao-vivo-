4e290b466112b0f9170fc35e662a701495bd2760
Lucasdardot <105593880+Lucasdardot@users.noreply.github.com>
2023-04-12 13:21:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+
+
+objects = []
+modelStatus = ""
+
+
+
+
+function setup(){
+    canvas = createCanvas(380,380)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.size(380,380)
+    video.hide()
+    
+}
+
+function start(){
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML = "status: Detectando objetos"
+}
+
+function modelLoaded(){
+    console.log("modelo Carregado!")
+    modelStatus = true
+    console.log(modelStatus)
+    
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error)
+    }
+    console.log(results)
+    objects = results
+}
+
+function draw(){
+    image(video,0,0,380,380)
+    
+
+    if(modelStatus !=""){
+        r = random(255)
+        g = random(255)
+        b = random(255)
+        objectDetector.detect(video, gotResult)
+        for (i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = " Status: objeto detectado"
+            document.getElementById("numberOfObjects").innerHTML = "quantidade de objetos detectados : " + objects.length
+            fill(r,g,b)
+            percent = floor(objects[i].confidence *100)
+            text(objects[i].label + "" + percent + "%", objects[i].x, objects[i].y)
+            noFill()
+            stroke(r,g,b)
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+        }
+    }
+}
